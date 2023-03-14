719c6cbe1250f38f0455604dcd8f9c2d5044f44a
ImashNipun <it21034404@my.sliit.lk>
2023-03-15 02:46:43 +0530

    Update HillCottageHotelServiceImp class

--- IT21034404/Hill Cottage Hotel Producer/src/hill_cottage_hotel_producer/HillCottageHotelServiceImp.java
+++ IT21034404/Hill Cottage Hotel Producer/src/hill_cottage_hotel_producer/HillCottageHotelServiceImp.java
@@ -14,24 +14,34 @@ public class HillCottageHotelServiceImp implements HillCottageHotelService{
 	public void generateReport(List<HillCottageBill> obj) {
 		
 		double totalAmount = 0;
+		double discount = 0;
+		double serviceCharge = 500.00;
 		
 		System.out.println("======================= Hill Cottage Hotel Bill =======================");
 		
-		System.out.format("%-18s %10s %25s %10s","Room Type","Number of Rooms","Number of days of stay","Amount");
+		System.out.format("%-18s %-10s %10s %25s %15s","Room Type","Price","Number of Rooms","Number of days of stay","Amount");
 		System.out.println();
 		System.out.println("==========================================================================");
 		
 		for(HillCottageBill bil : obj) {
 			double amount = calcTotalAmount(bil.getRoomAmount(),bil.getNumberOfRooms(),bil.getNumberOfDays());
-			System.out.format("%-18s %10s %25s %10s",bil.getRoomType(),bil.getNumberOfRooms(),bil.getNumberOfDays(),amount);
+			System.out.format("%-18s %-10s %10s %25s %15s",bil.getRoomType(),"Rs. "+bil.getRoomAmount(),bil.getNumberOfRooms(),bil.getNumberOfDays(),amount);
 			System.out.println();
+			
+			if(bil.getNumberOfDays()>=5) {
+				discount = (amount*10)/100.0;
+			}
 			
 			totalAmount += amount; 
 				
 		}
 		
 		System.out.println("==========================================================================");
-		System.out.format("%-18s %10s %10s %10s","","","","Total = "+totalAmount);
+		System.out.format("%-18s %-10s %10s %25s %15s","","","","","Service Charges = "+serviceCharge);
+		System.out.println();
+		System.out.format("%-18s %-10s %10s %25s %15s","","","","","Discount = "+discount);
+		System.out.println();
+		System.out.format("%-18s %-10s %10s %25s %15s","","","","","Total = "+((totalAmount+serviceCharge)-discount));
 		
 		
 	}
