7986dfbadb02b837a84b7ff6adb7ac5979d88629
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-03-17 21:57:08 +0530

    Transportation service is added

---
+++ IT21028878/Step it up Transportation Service Producer/src/step_it_up_transportation_service_producer/ItransportationServiceProducer.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package step_it_up_transportation_service_producer;
+
+import java.util.List;
+
+/**
+ * @author miule
+ *
+ */
+public interface ItransportationServiceProducer {
+	
+	public double transportationBillCalculator(int noOfDays, int vehicle, int driver);
+	public void displayBill(List<TransportationBill> obj);
+
+
+}
